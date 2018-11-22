8ba79ab3514089b8adc6a44f6e23673846c88cb6
tiktt <tikstt@gmail.com>
2018-11-23 00:33:01 +0700

    example

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,31 +2,20 @@ pipeline {
     agent any
 
     stages {
-        stage('git clone') {       
-          git clone "https://github.com/tikstt/laravel-unit-test-example.git"
-        }    
-    
-        stage ('Compile Stage') {
-            sh "composer install"
-            sh "cp .env.example .env"
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
         }
-
-        stage ('Testing Stage') {
-            // Run any testing suites
-            sh "./vendor/bin/phpunit"
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
         }
-
-
-        stage ('Deployment Stage') {
-            // If we had ansible installed on the server, setup to run an ansible playbook
-            // sh "ansible-playbook -i ./ansible/hosts ./ansible/deploy.yml"
-            sh "echo 'WE ARE DEPLOYING'"
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
         }
     }
-    
-    post { 
-        always { 
-            echo 'I will always say Hello again!'
-        }
-    }    
 }
