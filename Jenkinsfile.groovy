fdd1f5633968d1dba451c0301fe5f00bc465b54d
tiktt <tikstt@gmail.com>
2018-11-23 00:45:46 +0700

    change pipline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,20 +2,28 @@ pipeline {
     agent any
 
     stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
+    
+        stage ('Compile Stage') {
+            sh "cp .env.example .env"
+            sh "composer install --prefer-dist"
+            
         }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
+
+        stage ('Testing Stage') {
+            // Run any testing suites
+            sh "./vendor/bin/phpunit"
         }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
+
+        stage ('Deployment Stage') {
+            // If we had ansible installed on the server, setup to run an ansible playbook
+            // sh "ansible-playbook -i ./ansible/hosts ./ansible/deploy.yml"
+            sh "echo 'WE ARE DEPLOYING'"
         }
     }
+    
+    post { 
+        always { 
+            echo 'I will always say Hello again!'
+        }
+    }    
 }
