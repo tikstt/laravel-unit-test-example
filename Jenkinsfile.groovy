2804c3f8cebf33279341de37c667f0e0eff4e70b
tiktt <tikstt@gmail.com>
2018-11-23 00:24:10 +0700

    change Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,34 +1,22 @@
-#!/usr/bin/env groovy
+pipeline {
+    agent any
 
-node('master') {
-    try {
-        stage('build') {
-            // Checkout the app at the given commit sha from the webhook
-            checkout scm
-
-            // Install dependencies, create a new .env file and generate a new key, just for testing
+    stages {
+        stage ('Compile Stage') {
             sh "composer install"
             sh "cp .env.example .env"
-            //sh "php artisan key:generate"
-
-            // Run any static asset building, if needed
-            // sh "npm install && gulp --production"
         }
 
-        stage('test') {
+        stage ('Testing Stage') {
             // Run any testing suites
             sh "./vendor/bin/phpunit"
         }
 
-        stage('deploy') {
+
+        stage ('Deployment Stage') {
             // If we had ansible installed on the server, setup to run an ansible playbook
             // sh "ansible-playbook -i ./ansible/hosts ./ansible/deploy.yml"
             sh "echo 'WE ARE DEPLOYING'"
         }
-    } catch(error) {
-        throw error
-    } finally {
-        // Any cleanup operations needed, whether we hit an error or not
     }
-
 }
