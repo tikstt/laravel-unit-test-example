925ce3454c218b509659579094a3b29fee2309ac
tiktt <tikstt@gmail.com>
2018-11-26 02:17:15 +0700

    add sonar

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,33 +2,41 @@ pipeline {
     agent any
 
     stages {
-    
+
         stage ('Compile Stage') {
             steps {
               sh "cp .env.example .env"
               sh "composer install --prefer-dist"
-            }          
+            }
+        }
+
+        stage ('Sonar Stage') {
+            steps {
+              withSonarQubeEnv('Sonar'){
+                sh "sonar-scanner"
+              }              
+            }
         }
 
         stage ('Testing Stage') {
-            steps {        
+            steps {
               // Run any testing suites
               sh "./vendor/bin/phpunit"
             }
         }
 
         stage ('Deployment Stage') {
-            steps { 
+            steps {
               // If we had ansible installed on the server, setup to run an ansible playbook
               // sh "ansible-playbook -i ./ansible/hosts ./ansible/deploy.yml"
               sh "echo 'WE ARE DEPLOYING'"
             }
         }
     }
-    
-    post { 
-        always { 
+
+    post {
+        always {
             echo 'I will always say Hello again!'
         }
-    }    
+    }
 }
