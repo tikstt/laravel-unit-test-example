6bfc7a396e9bb08a935e8baa3ea4b21395f36022
tiktt <tikstt@gmail.com>
2018-11-27 00:56:41 +0700

    config groovy file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,13 +3,19 @@ pipeline {
 
     stages {
 
+        stage ('Git  Clone') {
+            steps {
+              git url: 'https://github.com/tikstt/laravel-unit-test-example.git/'
+            }
+        }
+
         stage ('Compile Stage') {
             steps {
               sh "cp .env.example .env"
               sh "composer install --prefer-dist"
             }
         }
-
+/*
         stage('SonarQube analysis') {
           steps {
             // requires SonarQube Scanner 2.8+
@@ -19,22 +25,28 @@ pipeline {
             }
           }
         }
-
-/*
-        stage ('Sonar Stage') {
-            steps {
-              withSonarQubeEnv('Sonar'){
-                sh "sonar-scanner"
-              }
-            }
-        }
 */
-        stage ('Testing Stage') {
+
+        stage ('Unit Testing Stage') {
             steps {
               // Run any testing suites
-              sh "./vendor/bin/phpunit"
+              sh "./vendor/bin/phpunit --log-junit 'reports/unitreport.xml'  "
             }
         }
+        
+        stage ('Load Test Stage') {
+            steps {
+              // Run any testing suites
+              sh "jmeter -n -t ./jmeter/kbtg.jmx -l ./jmeter/kbtg_result.jtl  -Jjmeter.save.saveservice.output_format=xml"
+              performanceReport parsers: [[$class: 'JMeterParser']],sourceDataFiles:"./jmeter/kbtg_result.jtl"
+              //performanceReport parsers: [[$class: 'JMeterParser', glob: "./jmeter/kbtg_result.jtl"]], errorFailedThreshold: 1, errorUnstableThreshold: 1, ignoreFailedBuild: false, ignoreUnstableBuild: false, relativeFailedThresholdNegative: 0, relativeFailedThresholdPositive: 0, relativeUnstableThresholdNegative: 0, relativeUnstableThresholdPositive: 0
+            }
+            //steps([$class: 'ArtifactArchiver', artifacts: './jmeter/kbtg_result.jtl'])
+        }        
+        
+        /*
+        ./vendor/bin/phpunit --log-junit 'reports/unitreport.xml'  --coverage-html 'reports/coverage' --coverage-clover 'reports/coverage/coverage.xml'
+        */
 
         stage ('Deployment Stage') {
             steps {
@@ -48,6 +60,7 @@ pipeline {
     post {
         always {
             echo 'I will always say Hello again!'
+             junit 'reports/unitreport.xml'
         }
     }
 }
