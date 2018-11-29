817a06a8ce865e4c6990b813f9becd5105e66159
tiktt <tikstt@gmail.com>
2018-11-30 03:08:07 +0700

    groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,25 +15,43 @@ pipeline {
               sh "composer install --prefer-dist"
             }
         }
-/*
-        stage('SonarQube analysis') {
-          steps {
-            // requires SonarQube Scanner 2.8+
-            //def scannerHome = tool 'SonarQube Scanner 2.8';
-            withSonarQubeEnv('jenkins-sonarqube') {
-              sh "${scannerHome}/bin/sonar-scanner"
-            }
-          }
-        }
-*/
 
         stage ('Unit Testing Stage') {
             steps {
               // Run any testing suites
-              sh "./vendor/bin/phpunit --log-junit 'reports/unitreport.xml'  "
+              sh "./vendor/bin/phpunit --log-junit './reports/unitreport.xml' --coverage-clover './reports/coverage.xml'    "
+              junit 'reports/unitreport.xml'
             }
         }
-        
+
+        stage('SonarQube analysis') {
+          steps {
+              withSonarQubeEnv('sonarcloud.io') {
+              sh "/home/ansiblegcp/sonar-scanner/bin/sonar-scanner -Dsonar.php.coverage.reportPaths=./reports/coverage.xml -Dsonar.php.tests.reportPath=./reports/unitreport.xml  -Dsonar.projectKey=tikstt_laravel-unit-test-example -Dsonar.organization=tikstt-github -Dsonar.sources=./app -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=530f6898d4e40eabbcc286d15dd4bb216364dec0"
+              }
+              /*
+            withSonarQubeEnv('jenkins-sonarqube') {
+              sh "${scannerHome}/bin/sonar-scanner"
+            }
+            */
+          }
+        }
+
+
+
+
+        stage ('RobotFramework Testing Stage') {
+            steps {
+                catchError {
+                   // sh "pybot ./robotframework/login_tests/valid_login.robot"
+                }
+                //currentBuild.result = 'SUCCESS'
+               // echo currentBuild.result
+
+            }
+        }
+
+
         stage ('Load Test Stage') {
             steps {
               // Run any testing suites
@@ -42,11 +60,8 @@ pipeline {
               //performanceReport parsers: [[$class: 'JMeterParser', glob: "./jmeter/kbtg_result.jtl"]], errorFailedThreshold: 1, errorUnstableThreshold: 1, ignoreFailedBuild: false, ignoreUnstableBuild: false, relativeFailedThresholdNegative: 0, relativeFailedThresholdPositive: 0, relativeUnstableThresholdNegative: 0, relativeUnstableThresholdPositive: 0
             }
             //steps([$class: 'ArtifactArchiver', artifacts: './jmeter/kbtg_result.jtl'])
-        }        
-        
-        /*
-        ./vendor/bin/phpunit --log-junit 'reports/unitreport.xml'  --coverage-html 'reports/coverage' --coverage-clover 'reports/coverage/coverage.xml'
-        */
+        }
+
 
         stage ('Deployment Stage') {
             steps {
@@ -60,7 +75,7 @@ pipeline {
     post {
         always {
             echo 'I will always say Hello again!'
-             junit 'reports/unitreport.xml'
+            // junit 'reports/unitreport.xml'
         }
     }
 }
