a9c3d446d098b74dbe404286168af47b41fb94f9
tiktt <tikstt@gmail.com>
2018-12-09 03:33:07 +0700

    sonar scan

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,7 +27,7 @@ pipeline {
         stage('SonarQube analysis') {
           steps {
               withSonarQubeEnv('sonarcloud.io') {
-              sh "/home/ansiblegcp/sonar-scanner/bin/sonar-scanner -Dsonar.php.coverage.reportPaths=./reports/coverage.xml -Dsonar.php.tests.reportPath=./reports/unitreport.xml  -Dsonar.projectKey=tikstt_laravel-unit-test-example -Dsonar.organization=tikstt-github -Dsonar.sources=./app -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=1c2905b8e9ea7825584b3a31280de1ed1903bef3"
+              sh "/home/ansiblegcp/sonar-scanner-3.2.0.1227-linux/bin/sonar-scanner -Dsonar.php.coverage.reportPaths=./reports/coverage.xml -Dsonar.php.tests.reportPath=./reports/unitreport.xml  -Dsonar.projectKey=tikstt_laravel-unit-test-example -Dsonar.organization=tikstt-github -Dsonar.sources=./app -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=1c2905b8e9ea7825584b3a31280de1ed1903bef3"
               }
               /*
             withSonarQubeEnv('jenkins-sonarqube') {
